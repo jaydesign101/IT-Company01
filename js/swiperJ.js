fae3f43d225b3df0b44702ac9b9d69513d4697ce
jaydesign <jaydesign101@gmail.com>
2023-04-21 00:22:56 +0900

    메인 화면 슬라이드 수정

--- js/swiperJ.js
+++ js/swiperJ.js
@@ -1,20 +1,17 @@
 var swiper = new Swiper(".swiper.mainbanner", {
   slidesPerView: 1,
   spaceBetween: 30,
-  loop: true,
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
+  centeredSlides: true,
+  // loop: true,
   pagination: {
     el: ".mainbanner.swiper-pagination",
-    type: "fraction",
-    //clickable: true,
+    // type: "fraction",
+    clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
   autoplay: {
     delay: 5000,
     disableOnInteraction: false
