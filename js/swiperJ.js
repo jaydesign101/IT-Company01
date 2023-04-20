5cbbfe905ccbb626e2f46607647dbd6a1433ccfe
jay <jaydesign101@gmail.com>
2023-04-20 16:51:58 +0900

    집에 갈래

--- js/swiperJ.js
+++ js/swiperJ.js
@@ -19,3 +19,16 @@ var swiper = new Swiper(".swiper.mainbanner", {
     disableOnInteraction: false
   },
 });
+
+var swiper = new Swiper(".news-right", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
