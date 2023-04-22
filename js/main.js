4f0ef5a9607ec7b1e89f955e2266d62723990d5e
jaydesign <jaydesign101@gmail.com>
2023-04-22 16:45:35 +0900

    scollMagic 효과

--- js/main.js
+++ js/main.js
@@ -115,3 +115,22 @@ document.querySelector('#roller2').style.left = weMakeEl.offsetWidth + 'px';//ro
 //클래스 할당
 weMakeEl.classList.add('original');
 clone.classList.add('clone');
+
+
+
+
+
+
+// 스크롤 애니메이션
+// 관리할 요소들 검색
+const spyEls = document.querySelectorAll('.scroll-spy')
+// 요소들 반복 처리!
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
